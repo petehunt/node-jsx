e1cd9b4182bf9be743d68bb5b0e9bfe5a367b96d
petehunt <floydophone@gmail.com>
2013-10-04 11:06:58 -0700

    it works

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var React = require('react-tools');
+
+var installed = false;
+
+function install() {
+  if (installed) {
+    return;
+  }
+
+  // Import everything in the transformer codepath before we add the import hook
+  React.transform('');
+
+  require.extensions['.js'] = function(module, filename) {
+    var src = fs.readFileSync(filename, {encoding: 'utf8'});
+    src = React.transform(src);
+    module._compile(src);
+  };
+
+  installed = true;
+}
+
+module.exports = {
+  install: install
+};
