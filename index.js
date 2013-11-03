3ebcc44faa453510f1ae93928665bf8cad270b96
Spike Brehm <ocelot@gmail.com>
2013-11-03 10:45:03 -0800

    Add 'options' arg to 'install', with 'extension' property

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var React = require('react-tools');
 
 var installed = false;
 
-function install() {
+function install(options) {
   if (installed) {
     return;
   }
 
+  options = options || {};
+
   // Import everything in the transformer codepath before we add the import hook
   React.transform('');
 
-  require.extensions['.js'] = function(module, filename) {
+  require.extensions[options.extension || '.js'] = function(module, filename) {
     var src = fs.readFileSync(filename, {encoding: 'utf8'});
     src = React.transform(src);
     module._compile(src, filename);
