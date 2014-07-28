97ccdecfe5e2075059b5bc421bfe68d5acf43059
Jaap Rood <jaap@jaaprood.nl>
2014-07-28 18:30:51 +1000

    Added 'addDocblock' options which will add the /** @jsx React.DOM */ comment to the source when it's missing like the 'reactify' module does

--- node-jsx.spec.js
+++ node-jsx.spec.js
@@ -1,6 +1,6 @@
 describe('node-jsx', function() {
   it('should work', function() {
-    require('./index').install();
+    require('./index').install({ addDocblock: true });
     expect(require('./test-module')).toBe('jonx');
   });
 });
