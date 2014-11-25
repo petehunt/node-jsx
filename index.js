ef23a7086e4c1089e95d541f1406bbaa34c57d66
petehunt <floydophone@gmail.com>
2014-11-24 16:05:51 -0800

    Revert "better pragma"
    This reverts commit 63a0904483f8f3bc441d76b65f2e0712daf355a9.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var React = require('react-tools');
 
 var installed = false;
 
-var DEFAULT_EXTENSION = '.js';
-
 function install(options) {
   if (installed) {
     return;
@@ -15,15 +13,13 @@ function install(options) {
   // Import everything in the transformer codepath before we add the import hook
   React.transform('', options);
 
-  var extension = options.extension || DEFAULT_EXTENSION;
-
-  require.extensions[extension] = function(module, filename) {
+  require.extensions[options.extension || '.js'] = function(module, filename) {
     var src = fs.readFileSync(filename, {encoding: 'utf8'});
     if (typeof options.additionalTransform == 'function') {
       src = options.additionalTransform(src);
     }
 
-    if (extension !== DEFAULT_EXTENSION || src.indexOf('@jsx') > -1) {
+    if (src.indexOf('@jsx') > -1) {
       try {
         src = React.transform(src, options);
       } catch (e) {
